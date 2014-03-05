7242c2693fbef13ee854d15ad444381074eafdd3
guigacarvalho <gcarvalho@scu.edu>
2014-03-05 14:34:07 -0800

    - Added some testing statements

--- src/gui/EcoReSystem.java
+++ src/gui/EcoReSystem.java
@@ -28,11 +28,15 @@ public class EcoReSystem {
 //            		station.getMachine(i).dropRandomRecyclableItem();
 //            		station.getMachine(i).dropRandomRecyclableItem();
             		station.getMachine(i).finishTransaction();
+            		
+            		            		            		
+            		station.getMachine(i).finishTransaction();
+            		
             	}
 //        		System.out.printf(station.toString());
-	               System.out.print("= RCM "+station.getMachine(0).location+" has "+station.getMachine(0).getTotalWeightOfMachine()+"kg and "+station.getMachine(0).listOfTransaction.size()+" transactions.\n");
-	               System.out.print("= RCM "+station.getMachine(1).location+" has "+station.getMachine(1).getTotalWeightOfMachine()+"kg and "+station.getMachine(1).listOfTransaction.size()+" transactions.\n");
 
+            	System.out.print("= RCM "+station.getMachine(0).location+" has "+station.getMachine(0).getTotalWeightOfMachine()+"kg and "+station.getMachine(0).listOfTransaction.size()+" transactions.\n");
+	            System.out.print("= RCM "+station.getMachine(1).location+" has "+station.getMachine(1).getTotalWeightOfMachine()+"kg and "+station.getMachine(1).listOfTransaction.size()+" transactions.\n");
 
             	AdminUI stationUI = new AdminUI(this.station);            	
             	stationUI.createAndShowGUI();
